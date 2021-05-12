e4208c099d3aac2786730ed59bc32da5ef785cf3
aestrad <abestrad@outlook.com>
2021-05-12 16:29:33 +0200

    2 files modified, 3 added

---
+++ src/app/shiftrostering/shiftrostering.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shiftrostering',
+  templateUrl: './shiftrostering.component.html',
+  styleUrls: ['./shiftrostering.component.scss']
+})
+export class ShiftRosteringComponent {}
